58f21d20e9fb4e438ef581f9c6fd24ef827b8e46
Loïc <lmarcx.pro@gmail.com>
2025-12-15 13:40:29 +0100

    Link for Post

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,19 +1,22 @@
-import { IPost } from "../../@types"
-import "./Post.scss"
+import { IPost } from "../../@types";
+import { Link } from "react-router";
+import "./Post.scss";
 
 interface PostProps {
-  post: IPost
+  post: IPost;
 }
 
 // rôle : afficher un article
 function Post({ post }: PostProps) {
   return (
     <article className="post">
-      <h2 className="post-title">{post.title}</h2>
-      <div className="post-category">{post.category}</div>
-      <p className="post-excerpt">{post.excerpt}</p>
+      <Link to={`/post/${post.title}`} className="post-link">
+        <h2 className="post-title">{post.title}</h2>
+        <div className="post-category">{post.category}</div>
+        <p className="post-excerpt">{post.excerpt}</p>
+      </Link>
     </article>
-  )
+  );
 }
 
-export default Post
+export default Post;
