7175643fb85d016d42ab41d78058c9152e718d43
Lanh Oclock <willanh.luangphavy@oclock.school>
2025-12-15 13:24:40 +0100

    mise en place du router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom/client"
 import App from "./components/App/App"
 import "./styles/index.scss"
 import { StrictMode } from "react"
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 )
